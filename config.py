738fe28b3a29bc21d96555ba8ba67def414566b0
Andy <103894785+AndyCryptoDev@users.noreply.github.com>
2022-06-19 21:40:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 WEBHOOK_HOST = '' #Your server IP address
 WEBHOOK_LISTEN = ''  #Your server IP address
 api_tel_bot  = '' #Telegram bot API key (from @botfather)
-wss = '' #websocket node from https://moralis.io 
+wss = 'https://bsc-dataseed.binance.org/' #You can use any convenient RPC Endpoint
 sender_address = '' #your wallet address
 private_key = ''#your private key from your wallet
