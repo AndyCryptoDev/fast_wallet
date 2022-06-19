05b552924f8b22bb8b922a0252339f906892312d
Andy <103894785+AndyCryptoDev@users.noreply.github.com>
2022-06-19 21:37:36 +0300

    Update installserverbash.sh

--- installserverbash.sh
+++ installserverbash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#First, let's update our linux
+#First, let's update our linux Ubuntu
 
 apt update
 apt list --upgradable
@@ -17,16 +17,22 @@ ufw status verbose
 
 apt install python3.8
 apt install python3-pip
-pip install pip --upgrade
-pip install setuptools --upgrade
-pip install web3
-pip install pytelegrambotapi
-pip install cherrypy
-pip install pysqlite3
+pip3 install pip --upgrade
+pip3 install setuptools --upgrade
+pip3 install web3
+pip3 install pytelegrambotapi
+pip3 install cherrypy
+pip3 install pysqlite3
+pip3 install binance-connector
+
+#Generation of self-signed SSL certificates
 
 apt-get install openssl
 openssl genrsa -out webhook_pkey.pem 2048
 openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
 
+#You should have 7 files in this folder after installing all libraries and dependencies
+
+ls -l
 
 
